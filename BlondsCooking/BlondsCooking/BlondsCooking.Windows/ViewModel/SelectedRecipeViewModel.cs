4ea243e9be41c52b719cc7541b733c9841bf5d47
agnieszkajach <agnieszka.jach@outlook.com>
2014-12-21 19:46:45 +0100

    Basic ViewModels, still seperate for two projects.

---
+++ BlondsCooking/BlondsCooking/BlondsCooking.Windows/ViewModel/SelectedRecipeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BlondsCooking.Model;
+using GalaSoft.MvvmLight;
+
+namespace BlondsCooking.ViewModel
+{
+    public class SelectedRecipeViewModel : ViewModelBase
+    {
+        private String selectedRecipe;
+        private Recipe _selectedRecipe;
+
+        public Recipe SelectedRecipe
+        {
+            get { return _selectedRecipe; }
+            set
+            {
+                _selectedRecipe = value;
+                RaisePropertyChanged(() => _selectedRecipe);
+            }
+        }
+    }
+}
---
+++ BlondsCooking/BlondsCooking/BlondsCooking.WindowsPhone/ViewModel/SelectedCategoryViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlondsCooking.Model;
+using GalaSoft.MvvmLight;
+
+namespace BlondsCooking.ViewModel
+{
+    public class SelectedCategoryViewModel : ViewModelBase
+    {
+        private String _selectedCategory;
+        private Recipe _selectedRecipe;
+        private ObservableCollection<Recipe> _recipesInSelectedCategory;
+
+        public Recipe SelectedRecipe
+        {
+            get { return _selectedRecipe; }
+            set
+            {
+                _selectedRecipe = value;
+                RaisePropertyChanged(() => _selectedRecipe);
+            }
+        }
+
+        public ObservableCollection<Recipe> RecipesInSelectedCategory
+        {
+            get { return _recipesInSelectedCategory; }
+            set
+            {
+                _recipesInSelectedCategory = value;
+                RaisePropertyChanged(() => RecipesInSelectedCategory);
+            }
+        }
+
+    }
+}
