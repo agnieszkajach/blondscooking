87a216ed70696e1ac3e3207b9f905c3f8e4b1736
agnieszkajach <agnieszka.jach@outlook.com>
2014-12-21 19:51:49 +0100

    NavigationMessage for Navigation between ViewModel and View.

---
+++ BlondsCooking/BlondsCooking/BlondsCooking.Shared/Common/NavigationMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace BlondsCooking.Common
+{
+    public class NavigationMessage : NotificationMessage
+    {
+        public string PageName
+        {
+            get { return base.Notification; }
+        }
+
+        public Dictionary<string, string> QueryStringParams { get; private set; }
+
+        public NavigationMessage(string pageName) : base(pageName) { }
+
+        public NavigationMessage(string pageName, Dictionary<string, string> queryStringParams) : this(pageName)
+        {
+            this.QueryStringParams = queryStringParams;
+        }
+    }
+}
